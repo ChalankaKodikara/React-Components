5da889445ef9545dca82ae9f2766c82add475616
chalankakodikara <ischalankakodikara@gmail.com>
2023-11-20 16:54:04 +0530

    Card

--- src/pages/page.js
+++ src/pages/page.js
@@ -1,15 +1,15 @@
 import React from 'react'
-import About from '../Slider/About.js'
+
 import Slider2 from '../Slider/Slider2.js'
 import './Page.css'
+import Card from '../components/card/Card.js'
 
 function page() {
   return (
     <div className='Container'>
       <div >
        <Slider2/>
-
-      <About/>
+<Card/>
       </div>
     </div>
   )
