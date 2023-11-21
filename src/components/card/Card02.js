046b6ceb1a3a5c718fd003e2175274a08eed8fc0
chalankakodikara <ischalankakodikara@gmail.com>
2023-11-21 17:27:10 +0530

    Landing Page

--- src/components/card/Card02.js
+++ src/components/card/Card02.js
@@ -3,24 +3,17 @@ import "./Card02.css";
 function card02() {
   return (
     <div>
-      <div class="card-container">
-        <div class="card">
-          <div class="front-content">
-            <p>Hover me</p>
-          </div>
-          <div class="content">
-            <p class="heading">Card Hover</p>
-            <p>
-              King Of Asia has a fine collection. With having knowledge of the
-              industry together with proud family traditions and respect and
-              almost notably passion has passed through generations. We assure
-              our customers receive the wide selection of high quality Museum
-              Gems and excellent service located at pioneer Museum GemS trading
-              place in, Sri Lanka.
-            </p>
-          </div>
-        </div>
-      </div>
+      <div class="card">
+  <div class="content">
+    <p class="heading">Card Hover Effect
+    </p><p class="para">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi
+      laboriosam at voluptas minus culpa deserunt delectus sapiente
+      inventore pariatur
+    </p>
+    <button class="btn">Read more</button>
+  </div>
+</div>
     </div>
   );
 }
